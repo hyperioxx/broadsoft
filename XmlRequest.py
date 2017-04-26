c842f68312669e7b5427def90dd0d9f2a82566d0
braiotta <braiotta@mit.edu>
2017-04-26 18:20:11 -0400

    to_xml is able to return XML skeleton

--- XmlRequest.py
+++ XmlRequest.py
@@ -1,29 +1,56 @@
+import xml.etree.ElementTree as ET
+
 class XmlRequest:
     def __init__(self):
-        self.action = 'add'
+        self.command_data = None
         self.command_name = None
         self.default_domain = 'broadworks'
         self.encoding = "ISO-8859-1"
+        self.protocol = 'OCI'
         self.session_id = None
         self.version = 1.0
+        self.xmlns = 'C'
+        self.xmlns_xsi = 'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"'
+
+    def to_xml(self):
+        self.validate()
+
+        bd = ET.Element('BroadsoftDocument')
+        bd.set('protocol', self.protocol)
+        bd.set('xmlns', self.xmlns)
+        bd.set('xmlns:xsi', self.xmlns_xsi)
+
+        sid = ET.SubElement(bd, 'sessionId')
+        sid.set('xmlns', '')
+        sid.text = str(self.session_id)
+
+        cmd = ET.SubElement(bd, 'command')
+        cmd.set('xsi:type', self.command_name)
+        cmd.set('xmlns', '')
+
+        return bd
+
+    def validate(self):
+        if self.command_name is None:
+            raise ValueError("can't run broadsoft.XmlRequest.to_xml() without a value for command_name")
 
     """
     <?xml version="1.0" encoding="ISO-8859-1"?>
     <BroadsoftDocument protocol="OCI" xmlns="C" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-    <sessionIdxmlns="">BB1A413DF12D404128F8956459FBD4D9</sessionId>
-    <command xsi:type="GroupAddRequest" xmlns="">
-    <serviceProviderId>sp1</serviceProviderId>
-    <groupId>group2</groupId>
-    <defaultDomain>broadworks</defaultDomain>
-    <userLimit>25</userLimit>
-    <groupName>Group 2</groupName>
-    <callingLineIdName>Group 2 Line ID</callingLineIdName>
-    <timeZone>America/New_York</timeZone>
-    <contact>
-    <contactName>Joe Smith</contactName>
-    <contactNumber>301-555-1212</contactNumber>
-    <contactEmail>joe.smith@broadworks.net</contactEmail>
-    </contact>
-    </command>
+        <sessionId xmlns="">BB1A413DF12D404128F8956459FBD4D9</sessionId>
+        <command xsi:type="GroupAddRequest" xmlns="">
+            <serviceProviderId>sp1</serviceProviderId>
+            <groupId>group2</groupId>
+            <defaultDomain>broadworks</defaultDomain>
+            <userLimit>25</userLimit>
+            <groupName>Group 2</groupName>
+            <callingLineIdName>Group 2 Line ID</callingLineIdName>
+            <timeZone>America/New_York</timeZone>
+            <contact>
+                <contactName>Joe Smith</contactName>
+                <contactNumber>301-555-1212</contactNumber>
+                <contactEmail>joe.smith@broadworks.net</contactEmail>
+            </contact>
+        </command>
     </BroadsoftDocument>
     """
--- tests/test_xml.py
+++ tests/test_xml.py
@@ -1,6 +1,27 @@
 import unittest
 import unittest.mock
+import xml.etree.ElementTree as ET
+from broadsoft.XmlRequest import XmlRequest
 
 class TestBroadsoftXML(unittest.TestCase):
-    def test_search_type_adds_search_elements(self):
-        self.assertFalse("write this; probably XML_search extends XML, and then specific searches extend that")
+    def test_to_xml(self):
+        x = XmlRequest()
+        x.command_name = 'GroupAddRequest'
+        x.default_domain = 'dd'
+        x.encoding = 'piglatin'
+        x.protocol = 'gopher'
+        x.session_id = 'seshy'
+        x.version = '2.5'
+        x.xmlns = 'PG13'
+        x.xmlns_xsi = "http://youtube.com"
+
+        xml = x.to_xml()
+        self.assertEqual(
+            '<BroadsoftDocument protocol="gopher" xmlns="PG13" xmlns:xsi="http://youtube.com"><sessionId xmlns="">seshy</sessionId><command xmlns="" xsi:type="GroupAddRequest" /></BroadsoftDocument>',
+            ET.tostring(element=xml).decode("utf-8")
+        )
+
+    def test_to_xml_requires_command(self):
+        x = XmlRequest()
+        with self.assertRaises(ValueError):
+            x.to_xml()
