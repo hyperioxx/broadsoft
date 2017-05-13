3cc1e7b07a38fb1aef21ddaeec7b0f610d48e76b
braiotta <braiotta@mit.edu>
2017-05-12 20:42:54 -0400

    UserSharedCallAppearanceModifyRequest works

--- requestobjects/UserSharedCallAppearanceModifyRequest.py
+++ requestobjects/UserSharedCallAppearanceModifyRequest.py
@@ -6,16 +6,67 @@ class UserSharedCallAppearanceModifyRequest(BroadsoftRequest):
     command_name = 'UserSharedCallAppearanceModifyRequest'
     check_success = True
 
-    def __init__(self, **kwargs):
-        """
-        <userId>mituser1@broadsoft-dev.mit.edu</userId>
-        <alertAllAppearancesForClickToDialCalls>true</alertAllAppearancesForClickToDialCalls>
-        <alertAllAppearancesForGroupPagingCalls>false</alertAllAppearancesForGroupPagingCalls>
-        <allowSCACallRetrieve>true</allowSCACallRetrieve>
-        <multipleCallArrangementIsActive>true</multipleCallArrangementIsActive>
-        <allowBridgingBetweenLocations>false</allowBridgingBetweenLocations>
-        <bridgeWarningTone>None</bridgeWarningTone>
-        <enableCallParkNotification>false</enableCallParkNotification>
-        </command>
-        """
+    booleans = [
+        'alert_all_appearances_for_click_to_dial_calls',
+        'alert_all_appearances_for_group_paging_calls',
+        'allow_sca_call_retrieve',
+        'multiple_call_arrangement_is_active',
+        'allow_bridging_between_locations',
+        'enable_call_park_notification'
+    ]
+
+    def __init__(self, sip_user_id=None, alert_all_appearances_for_click_to_dial_calls=True,
+                 alert_all_appearances_for_group_paging_calls=False, allow_sca_call_retrieve=True,
+                 multiple_call_arrangement_is_active=True, allow_bridging_between_locations=False,
+                 bridge_warning_tone='None', enable_call_park_notification=False, did=None,
+                 **kwargs):
+        self.did=did
+        self.sip_user_id = sip_user_id
+        self.alert_all_appearances_for_click_to_dial_calls = alert_all_appearances_for_click_to_dial_calls
+        self.alert_all_appearances_for_group_paging_calls = alert_all_appearances_for_group_paging_calls
+        self.allow_sca_call_retrieve = allow_sca_call_retrieve
+        self.multiple_call_arrangement_is_active = multiple_call_arrangement_is_active
+        self.allow_bridging_between_locations = allow_bridging_between_locations
+        self.bridge_warning_tone = bridge_warning_tone
+        self.enable_call_park_notification = enable_call_park_notification
         BroadsoftRequest.__init__(self, **kwargs)
+        if self.did and self.sip_user_id is None:
+            self.sip_user_id = self.derive_sip_user_id()
+
+    def build_command_xml(self):
+        if self.did and not self.sip_user_id:
+            self.sip_user_id = self.derive_sip_user_id()
+        self.validate()
+
+        cmd = self.build_command_shell()
+
+        uid = ET.SubElement(cmd, 'userId')
+        uid.text = self.sip_user_id
+
+        s = ET.SubElement(cmd, 'alertAllAppearancesForClickToDialCalls')
+        s.text = self.alert_all_appearances_for_click_to_dial_calls
+
+        s = ET.SubElement(cmd, 'alertAllAppearancesForGroupPagingCalls')
+        s.text = self.alert_all_appearances_for_group_paging_calls
+
+        s = ET.SubElement(cmd, 'allowSCACallRetrieve')
+        s.text = self.allow_sca_call_retrieve
+
+        s = ET.SubElement(cmd, 'multipleCallArrangementIsActive')
+        s.text = self.multiple_call_arrangement_is_active
+
+        s = ET.SubElement(cmd, 'allowBridgingBetweenLocations')
+        s.text = self.allow_bridging_between_locations
+
+        s = ET.SubElement(cmd, 'bridgeWarningTone')
+        s.text = self.bridge_warning_tone
+
+        s = ET.SubElement(cmd, 'enableCallParkNotification')
+        s.text = self.enable_call_park_notification
+
+        return cmd
+
+    def validate(self):
+        if self.sip_user_id is None:
+            raise ValueError("can't run broadsoft.UserSharedCallAppearanceModifyRequest.to_xml() without a value for sip_user_id.")
+
--- tests/requestobjects/test_UserSharedCallAppearanceModifyRequest.py
+++ tests/requestobjects/test_UserSharedCallAppearanceModifyRequest.py
@@ -1,8 +1,74 @@
 import unittest.mock
 import xml.etree.ElementTree as ET
 from broadsoft.requestobjects.UserSharedCallAppearanceModifyRequest import UserSharedCallAppearanceModifyRequest
+from broadsoft.requestobjects.lib.BroadsoftRequest import BroadsoftRequest
 
 
-class TestBroadsoftUserAddRequest(unittest.TestCase):
-    def test_stub(self):
-        self.assertFalse("this is the last piece of user provisioning; build it out, then extend a method to provision a user")
+class TestBroadsoftUserSharedCallAppearanceModifyRequest(unittest.TestCase):
+    def test_validate(self):
+        u = UserSharedCallAppearanceModifyRequest()
+        with self.assertRaises(ValueError):
+            u.validate()
+
+    @unittest.mock.patch.object(UserSharedCallAppearanceModifyRequest, 'validate')
+    def test_build_command_xml_calls_validate(
+            self,
+            validate_patch
+    ):
+        u = UserSharedCallAppearanceModifyRequest(sip_user_id='6175551212@broadsoft-dev.mit.edu')
+        u.build_command_xml()
+        self.assertTrue(validate_patch.called)
+
+    def test_sip_userid_derived_from_did(self):
+        u = UserSharedCallAppearanceModifyRequest(did='617-555-1212')
+        self.assertEqual('6175551212@' + u.default_domain, u.sip_user_id)
+
+        u = UserSharedCallAppearanceModifyRequest(sip_user_id='beaver@mit.edu', did='617-555-1212')
+        self.assertEqual('beaver@mit.edu', u.sip_user_id)
+
+    @unittest.mock.patch.object(BroadsoftRequest, 'derive_sip_user_id')
+    def test_build_command_xml_derives_userid(
+            self,
+            derive_sip_user_id_patch
+    ):
+        u = UserSharedCallAppearanceModifyRequest()
+        self.assertFalse(derive_sip_user_id_patch.called)
+        u.did = '617-555-1212'
+        u.build_command_xml()
+        self.assertTrue(derive_sip_user_id_patch.called)
+
+        derive_sip_user_id_patch.called = False
+        u = UserSharedCallAppearanceModifyRequest()
+        self.assertFalse(derive_sip_user_id_patch.called)
+        u.did = '617-555-1212'
+        u.sip_user_id = 'beaver@mit.edu'
+        u.build_command_xml()
+        self.assertFalse(derive_sip_user_id_patch.called)
+
+    def test_build_command_xml(self):
+        u = UserSharedCallAppearanceModifyRequest(did=6175551212,
+                                                  alert_all_appearances_for_click_to_dial_calls=False,
+                                                  alert_all_appearances_for_group_paging_calls=True,
+                                                  allow_sca_call_retrieve=False,
+                                                  multiple_call_arrangement_is_active=False,
+                                                  allow_bridging_between_locations=True,
+                                                  bridge_warning_tone='Awooga',
+                                                  enable_call_park_notification=True)
+        target_xml = \
+            '<command xmlns="" xsi:type="UserSharedCallAppearanceModifyRequest">' + \
+            '<userId>6175551212@' + u.default_domain + '</userId>' + \
+            '<alertAllAppearancesForClickToDialCalls>false</alertAllAppearancesForClickToDialCalls>' + \
+            '<alertAllAppearancesForGroupPagingCalls>true</alertAllAppearancesForGroupPagingCalls>' + \
+            '<allowSCACallRetrieve>false</allowSCACallRetrieve>' + \
+            '<multipleCallArrangementIsActive>false</multipleCallArrangementIsActive>' + \
+            '<allowBridgingBetweenLocations>true</allowBridgingBetweenLocations>' + \
+            '<bridgeWarningTone>Awooga</bridgeWarningTone>' + \
+            '<enableCallParkNotification>true</enableCallParkNotification>' + \
+            '</command>'
+
+        xml = u.to_xml()
+        cmd = xml.findall('./command')[0]
+        self.assertEqual(
+            target_xml,
+            ET.tostring(cmd).decode('utf-8')
+        )
