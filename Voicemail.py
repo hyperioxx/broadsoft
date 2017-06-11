c656444e8fffc4a4e68f97b7231e698e1059a69b
braiotta <braiotta@mit.edu>
2017-06-11 16:33:07 -0400

    added Voicemail object

---
+++ Voicemail.py
@@ -0,0 +1,93 @@
+from broadsoft.lib.BroadsoftObject import BroadsoftObject
+from broadsoft.requestobjects.UserVoiceMessagingUserModifyVoiceManagementRequest import UserVoiceMessagingUserModifyVoiceManagementRequest
+from broadsoft.requestobjects.UserThirdPartyVoiceMailSupportModifyRequest import UserThirdPartyVoiceMailSupportModifyRequest
+
+
+class Voicemail(BroadsoftObject):
+    unity_number = 6172530000
+
+    def __init__(self, behavior='email', busy_to_voicemail=True, cc_email=None, did=None, email=None, mwi=None,
+                 no_answer_to_voicemail=True, rings=3, straight_to_voicemail=False, send_cc=False, sip_user_id=None,
+                 type='broadsoft', transfer_on_zero=False, transfer_number=None,
+                 **kwargs):
+        self.behavior = behavior                            # "email" or "store" (on server)
+        self.busy_to_voicemail = busy_to_voicemail
+        self.cc_email = cc_email
+        self.did = did
+        self.email = email
+        self.mwi = mwi
+        self.no_answer_to_voicemail = no_answer_to_voicemail
+        self.rings = rings
+        self.send_cc = send_cc
+        self.sip_user_id = sip_user_id
+        self.straight_to_voicemail = straight_to_voicemail
+        self.transfer_on_zero = transfer_on_zero
+        self.transfer_number = transfer_number
+        self.type = type                                    # broadsoft or unity
+
+        BroadsoftObject.__init__(self, **kwargs)
+
+    def build_activate_command(self):
+        self.validate()
+
+        if self.type == 'broadsoft':
+            return self.build_activate_command__broadsoft()
+        elif self.type == 'unity':
+            return self.build_activate_command__unity()
+        else:
+            raise NotImplementedError("no Voicemail.build_activate_command() behavior defined for type " + str(self.type))
+
+    def build_activate_command__broadsoft(self):
+        # configure the XML object that activates broadsoft voicemail
+        activate = UserVoiceMessagingUserModifyVoiceManagementRequest()
+        activate.sip_user_id = self.sip_user_id
+        activate.is_active = True
+        activate.processing = 'Deliver To Email Address Only'
+        activate.voice_message_delivery_email_address = self.email
+        activate.use_phone_message_waiting_indicator = self.mwi
+        activate.send_voice_message_notify_email = self.email
+        activate.voice_message_notify_email_address = self.email
+        activate.send_carbon_copy_voice_message = self.send_cc
+        activate.voice_message_carbon_copy_email_address = self.cc_email
+        activate.transfer_on_zero_to_phone_number = self.transfer_on_zero
+        activate.transfer_phone_number = self.transfer_number
+        activate.always_redirect_to_voice_mail = self.straight_to_voicemail
+        activate.busy_redirect_to_voice_mail = self.busy_to_voicemail
+        activate.no_answer_redirect_to_voice_mail = self.no_answer_to_voicemail
+        activate.out_of_primary_zone_redirect_to_voice_mail = False
+
+        return activate
+
+    def build_activate_command__unity(self):
+        # configure the XML object that activates unity voicemail
+        activate = UserThirdPartyVoiceMailSupportModifyRequest()
+        activate.sip_user_id = self.sip_user_id
+        activate.is_active = True
+        activate.busy_redirect_to_voice_mail = self.busy_to_voicemail
+        activate.no_answer_redirect_to_voice_mail = self.no_answer_to_voicemail
+        activate.user_server = self.unity_number
+        activate.mailbox_id_type = 'User Or Group Phone Number'
+        activate.no_answer_number_of_rings = self.rings
+        activate.always_redirect_to_voice_mail = self.straight_to_voicemail
+        activate.out_of_primary_zone_redirect_to_voice_mail = False
+
+        return activate
+
+    def build_deactivate_counterpart_command(self):
+        if self.type == 'unity':
+            # if we're activating unity, deactivate broadsoft
+            return UserVoiceMessagingUserModifyVoiceManagementRequest(sip_user_id=self.sip_user_id, is_active=False)
+
+        elif self.type == 'broadsoft':
+            # if we're activating broadsoft, deactivate third party
+            return UserThirdPartyVoiceMailSupportModifyRequest(sip_user_id=self.sip_user_id, is_active=False)
+
+        else:
+            raise NotImplementedError("no Voicemail.build_deactivate_counterpart_command() behavior defined for type " + str(self.type))
+
+    def validate(self):
+        if not self.sip_user_id:
+            raise ValueError("can't call Account.activate_unity_voicemail without a value for sip_user_id")
+
+        if not self.email:
+            raise ValueError("can't call Account.activate_unity_voicemail without a value for email")
---
+++ tests/test_voicemail.py
@@ -0,0 +1,66 @@
+import unittest.mock
+from broadsoft.Voicemail import Voicemail
+from broadsoft.lib.BroadsoftObject import BroadsoftObject
+from broadsoft.lib import BroadsoftInstance
+from xml.etree.ElementTree import Element
+import xml.etree.ElementTree as ET
+from broadsoft.requestobjects.UserVoiceMessagingUserModifyVoiceManagementRequest import UserVoiceMessagingUserModifyVoiceManagementRequest
+from broadsoft.requestobjects.UserThirdPartyVoiceMailSupportModifyRequest import UserThirdPartyVoiceMailSupportModifyRequest
+
+
+def return_none(**kwargs):
+    return None
+
+
+class TestBroadsoftVoicemail(unittest.TestCase):
+    def test_init(self):
+        v = Voicemail(behavior='b', busy_to_voicemail='btv', cc_email='cce', did='617-555-1212', email='e',
+                      mwi='m', no_answer_to_voicemail='natv', rings='r', straight_to_voicemail='stv', send_cc='scc',
+                      sip_user_id='6175551212@mit.edu', type='broadsoftttt', transfer_on_zero='toz',
+                      transfer_number='tn')
+        self.assertEqual('b', v.behavior)
+        self.assertEqual('btv', v.busy_to_voicemail)
+        self.assertEqual('cce', v.cc_email)
+        self.assertEqual('6175551212', v.did)
+        self.assertEqual('e', v.email)
+        self.assertEqual('m', v.mwi)
+        self.assertEqual('natv', v.no_answer_to_voicemail)
+        self.assertEqual('r', v.rings)
+        self.assertEqual('stv', v.straight_to_voicemail)
+        self.assertEqual('scc', v.send_cc)
+        self.assertEqual('6175551212@mit.edu', v.sip_user_id)
+        self.assertEqual('broadsoftttt', v.type)
+        self.assertEqual('toz', v.transfer_on_zero)
+        self.assertEqual('tn', v.transfer_number)
+
+    def test_type_determines_correct_activation(self):
+        v = Voicemail(sip_user_id='6175551212@beaver.mit.edu', email='beaver@mit.edu')
+        v.type = 'broadsoft'
+        activate = v.build_activate_command()
+        deactivate = v.build_deactivate_counterpart_command()
+        self.assertIsInstance(activate, UserVoiceMessagingUserModifyVoiceManagementRequest)
+        self.assertIsInstance(deactivate, UserThirdPartyVoiceMailSupportModifyRequest)
+
+        v = Voicemail(sip_user_id='6175551212@beaver.mit.edu', email='beaver@mit.edu')
+        v.type = 'unity'
+        activate = v.build_activate_command()
+        deactivate = v.build_deactivate_counterpart_command()
+        self.assertIsInstance(activate, UserThirdPartyVoiceMailSupportModifyRequest)
+        self.assertIsInstance(deactivate, UserVoiceMessagingUserModifyVoiceManagementRequest)
+
+        v = Voicemail(sip_user_id='6175551212@beaver.mit.edu', email='beaver@mit.edu')
+        v.type = 'garbanzo'
+        with self.assertRaises(NotImplementedError):
+            v.build_activate_command()
+
+        with self.assertRaises(NotImplementedError):
+            v.build_deactivate_counterpart_command()
+
+    def test_validate(self):
+        v = Voicemail(email='beaver@mit.edu')
+        with self.assertRaises(ValueError):
+            v.validate()
+
+        v = Voicemail(sip_user_id='6175551212@beaver.mit.edu')
+        with self.assertRaises(ValueError):
+            v.validate()
