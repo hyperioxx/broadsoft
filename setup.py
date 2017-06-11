092b430bce65adeaf351861c1287fdbdc3ed4f34
braiotta <braiotta@mit.edu>
2017-06-11 16:05:46 -0400

    added setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='broadsoft',
+    version='0.0.1',
+    description='tools for speaking to the Broadsoft cloud-based VOIP service',
+    url='https://github.mit.edu/nist/unity',
+    author='Chris Braiotta',
+    author_email='braiotta@mit.edu',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=['requests']
+)
