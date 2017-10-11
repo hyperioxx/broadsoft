1decd7d7a7806cafe149c71e96204227e33a3405
braiotta <braiotta@mit.edu>
2017-10-11 13:45:41 -0400

    updated list of default services

--- broadsoft/tests/test_account.py
+++ broadsoft/tests/test_account.py
@@ -880,7 +880,7 @@ class TestBroadsoftAccount(unittest.TestCase):
 
     def test_default_services(self):
         a = Account()
-        self.assertEqual(112, len(a.load_default_services()))
+        self.assertEqual(108, len(a.load_default_services()))
 
     def test_default_service_pack(self):
         # going with individual services, not service pack
